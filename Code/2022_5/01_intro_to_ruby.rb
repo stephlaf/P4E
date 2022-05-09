df7488aed70a2d85db66e1be56ebd4aa7cd06a15
Kieran Dunch <dunch.kieran@gmail.com>
2022-05-09 13:11:42 -0400

    First module finished.

--- Code/2022_5/01_intro_to_ruby.rb
+++ Code/2022_5/01_intro_to_ruby.rb
@@ -1,1 +1,96 @@
 # Welcome to P4E! 🥳
+
+# puts and print
+
+# prints to the console without a new line
+ print "Hello world!"
+ print "Hello world!!!!!!!"
+
+# prints to the console with the addition of a new line
+ puts 6
+ puts "Testing testing!"
+
+# data types
+
+# string: denoted with ""
+
+"Hello"
+
+# Numbers: Two types: integer and float
+
+# Integer
+3
+
+# Float
+3.5
+
+# Booleans: True or False (more on this in Module 2)
+true
+false
+
+# Variables: declared using =, stores useful and repeatable information
+
+name = "Ricardo Gutierrez"
+
+# outputting the value of the variable to console
+puts name
+
+#  when declaring variables in Ruby use lower_snake_case
+
+# Math operations!
+
+# addition
+puts 3 + 3
+
+# subtraction
+puts 3 - 4
+
+# multiplication
+puts 3 * 4
+
+# division
+puts 10 / 5
+
+# exponentiation
+puts 2 ** 3
+
+#  modulo: provides the remainder of a division
+puts 12 % 9
+
+
+# Single line and multi-line comments!
+# commenting allows you to annotate your code, making it more understandable
+
+# Single line comment
+
+# Multi-line comment
+
+=begin
+name = "charu"
+
+puts name
+
+puts name.capitalize
+
+puts name.upcase!
+
+puts name
+
+=end
+
+# String interpolation: Executing ruby code inside of a string!
+
+name = "Leo Angelo Bacolores"
+
+puts "Hello, my name is #{name}"
+
+
+# Getting user data: gets.chomp takes input data from the user!
+
+puts "What is your favourite colour?"
+
+answer = gets.chomp
+
+puts "Your favourite colour is #{answer}! This is another sentence."
+
+# welcome everyone!
